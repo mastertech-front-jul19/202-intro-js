4f8930af44fffcdcdfb4f520f42ebf7ccf3d3009
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-07-11 20:56:32 -0300

    Adicionar primeiros exemplos de aula

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+TABELA VERDADE
+
+&& (AND)
+true && true == true
+true && false == false
+false && false == false
+
+|| (OR)
+true || true == true
+true || false == true
+false || false == false
+
+CAMMEL CASING
+Nome da Variável
+nomeDaVariavel
+*/
+
+console.log('Hello World');
+
+let nomeDoUsuario = prompt('Qual o nome do usuário?');
+let idade = prompt('Qual é a sua idade?');
+let programador = confirm('Você é um programador?');
+
+alert(nomeDoUsuario);
+console.log(idade);
+console.log(programador);
